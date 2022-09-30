fd7ce12f5e7a76915ca948b6d561395051f2ef65
SergioUNAM <hhser@comunidad.unam.mx>
2022-09-29 23:01:50 -0500

    Tareas

--- u2_act1_infv.py
+++ u2_act1_infv.py
@@ -29,7 +29,7 @@ class Movil:
     # Metodo constructor predefinido
 
     on_off_estado = False
-
+    reconocimiento_facial = False
     def __init__(self, marca, modelo, color, precio, camara, procesador, huella_digital, tipo_sensores):
         self.__marca = marca
         self.__modelo = modelo
@@ -57,7 +57,7 @@ class Movil:
         return self.__modelo
 
     @modelo.setter
-    def set_modelo(self, value):
+    def modelo(self, value):
         self.__modelo = value
 
     # color
@@ -66,7 +66,7 @@ class Movil:
         return self._color
 
     @color.setter
-    def set_color(self, value):
+    def color(self, value):
         self.__color = value
 
     # precio
@@ -75,7 +75,7 @@ class Movil:
         return self._precio
 
     @precio.setter
-    def set_precio(self, value):
+    def precio(self, value):
         self.__precio = value
 
     # camara
@@ -84,7 +84,7 @@ class Movil:
         return self.__camara
 
     @camara.setter
-    def set_camara(self, value):
+    def camara(self, value):
         self.__camara = value
 
     # procesador
@@ -93,7 +93,7 @@ class Movil:
         return self.__procesador
 
     @procesador.setter
-    def set_procesador(self, value):
+    def procesador(self, value):
         self.__procesador = value
 
     # huella_digital
@@ -102,7 +102,7 @@ class Movil:
         return self.__huella_digital
 
     @huella_digital.setter
-    def set_huella_digital(self, value):
+    def huella_digital(self, value):
         self.__huella_digital = value
 
     # tipo_sensores
@@ -111,7 +111,7 @@ class Movil:
         return self.__tipo_sensores
 
     @tipo_sensores.setter
-    def set_tipo_sensores(self, value):
+    def tipo_sensores(self, value):
         self.__tipo_sensores = value
 
     # Métodos de la clase Movil
@@ -132,55 +132,92 @@ class Movil:
             print(f"{self.marca} {self.modelo} está encendido")
         else:
             print(f"{self.marca} {self.modelo} está encendido")
+        print()
 
     def apagar(self):
         if self.on_off_estado:
-            not self.on_off_estado
+            self.on_off_estado = not self.on_off_estado
             print(f"{self.__marca} {self.__modelo}, apagado")
+        print()
 
-    def usarSensor(self):
-        contador = 1
-        for sensor in self.__tipo_sensores:
-            print(f"{contador}.- {sensor}")
-            contador += 1
+    def usarSensor(self, sensor):
+        if sensor == 1:
+            print(f"Utilizando el sensor {self.tipo_sensores[sensor - 1]}, modificando brillo")
+        elif sensor == 2:
+            print(f"Utilizando el sensor {self.tipo_sensores[sensor - 1]}, girando pantalla")
+
+        elif sensor == 3:
+            print(f"Utilizando el sensor {self.tipo_sensores[sensor - 1]}, desbloquendo teléfono")
+        print()
 
     def tomarFoto(self):
-        pass
+        print("Iniciando cámara")
+        print("Capturando imagen")
+        print("Guardando imagen")
+        print()
 
-    def realizarLlamada(self, num_telefono):
-        pass
+    def realizarLlamada(self):
+        num_telefono = input("Digite el telefono al que se desea marcar")
+        print(f"Llamando al número {num_telefono}")
+        print()
 
-    def bloquear(self, contraseña):
-        pass
+    def bloquear(self):
+        print("Bloqueando teléfono")
+        print("Apagando pantalla")
+        print()
 
     def desbloquear(self):
-        pass
+        if self.huella_digital == False:
+            self.usarSensor(3)
+        else:
+            print("Utilizando sensor de huella digital")
+        print()
 
-    def abrirApp(self):
-        pass
+    def abrirApp(self, nombreApp):
+        print(f"Abriendo {nombreApp}")
+        print()
 
-    def subeVolumen(self):
-        pass
+    def subeVolumen(self, niveles):
+        print(f"Subiendo el volumen {niveles} niveles")
+        print()
 
-    def bajaVolumen(self):
-        pass
+    def bajaVolumen(self, niveles):
+        print(f"Bajando el volumen {niveles} niveles")
+        print()
 
 
 # Entry point, desde donde se ejecuta el programa principal
 if __name__ == "__main__":
-    Apple = Movil("Apple", "11", "blanco", 10999, "12 MP", "A13 Bionic", False,
+    Apple = Movil("Apple", "Iphone 11", "blanco", 10999, "12 MP", "A13 Bionic", False,
                   ["Luz ambiental", "Giroscopio", "Reconocimiento Facial"])
-    # Apple.muestra_informacion()
+
+
+    # Funciones especificas iphone
+    #La manera correcta de implentar las funciones especificas es creando una nueva clase para los telefonos apple y aplicando la herencia de la clase general Movil, para el alcance de este ejercicio se hara de esta forma
+    def cambio_de_luz_iphone():
+        Apple.usarSensor(1)
+
+    def girar_pantalla_iphone():
+        Apple.usarSensor(2)
+
+    # Utilizando los métodos solicitados
     Apple.encender()
     Apple.apagar()
-    Apple.usarSensor()
-    Apple.get_tipo_sensores()
+    Apple.desbloquear()
+    Apple.tomarFoto()
+    cambio_de_luz_iphone()
+    girar_pantalla_iphone()
+    Apple.realizarLlamada()
+    Apple.abrirApp("Facebook")
+    Apple.subeVolumen(3)
+    Apple.bajaVolumen(4)
 
+
+    #Teléfono 2
     Samsung = Movil("Samsung", "A13 4G", "Negro", 3600, "50 MP", "Octa core de 2,2GHz", True,
                     ["Acelerometro, Giroscopio"])
-    # Samsung.muestra_informacion()
 
+    #Teléfono 3
     Xiaomi = Movil("Xiaomi", "Redmi note 8", "Negro", 3600, "48 MP", "Qualcomm Snapdragon 665", True,
                    ["Sensor de proximidad", "Sensor de luz", "Acelerometro", "Giroscopio",
                     "Lector de huellas dactilares"])
-    # Xiaomi.muestra_informacion()
