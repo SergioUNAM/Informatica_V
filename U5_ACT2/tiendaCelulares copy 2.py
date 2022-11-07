5d0444d4517e5570980f586116991c73f8dbc22b
SergioUNAM <hhser@comunidad.unam.mx>
2022-11-06 22:41:50 -0600

    Abre la ventana y asigna bien los valores de la marca al objeto teléfono

--- U5_ACT2/tiendaCelulares copy 2.py
+++ U5_ACT2/tiendaCelulares copy 2.py
@@ -1,7 +1,6 @@
 import tkinter as tk
-from tkinter import ttk, messagebox
-from tkinter.messagebox import showinfo
-from turtle import title
+from tkinter import ttk
+
 
 
 # Creamos la clase TelefonoCelular con los siguientes atrbutos privados: marca, modelo, color, capacidad de almacenamiento, precio, plan_renta, seguro, precio_total
@@ -95,73 +94,45 @@ class TelefonoCelular:
     def __str__(self):
         return f"Marca: {self.__marca}, Modelo: {self.__modelo}, Color: {self.__color}, Capacidad de almacenamiento: {self.__capacidad_almacenamiento}, Precio: {self.__precio}, Plan de renta: {self.__plan_renta}, Seguro: {self.__seguro}, Precio total: {self.__precio_total}"
 
+class App(tk.Frame):
+    def __init__(self,parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.combo = ttk.Combobox(self, values=["Samsung", "Apple", "Huawei", "Xiaomi", "Motorola"])
+        self.combo2 = ttk.Combobox(self)
+        self.combo.grid(row=0, column=0)
+        self.combo2.grid(row=1, column=0)
+        self.combo.bind("<<ComboboxSelected>>", self.combo2_update)
 
-# Creamos la función Interfaz1
-def Interfaz1(telefono):
-    # Creamos la ventana
-    def ventana():
-        ventana = tk.Tk()
-        ventana.geometry('500x500')
-        ventana.resizable(False, False)
-        ventana.title('Tienda de Celulares')
-
-        # Creamos un titulo con formato atractivo
-        etiqueta = tk.Label(ventana, text='Tienda de Celulares', font=('Arial', 20, 'bold'))
-        etiqueta.pack(fill=tk.X, padx=5, pady=5)
-
+    def combo2_update(self, event):
+        now = self.combo2.get()
+        if (combo_sel:=self.combo.get()) == "Samsung":
+                values=["Galaxy S21", "Galaxy S21+", "Galaxy S21 Ultra"]
+        elif combo_sel == "Apple":
+                values=["iPhone 12", "iPhone 12 Pro", "iPhone 12 Pro Max"]
+        else:
+            values = ()
         
-
-        # Ejecutamos los combos solicitados
-        comboMarca(ventana)
-        comboModelo(ventana)
-
-
-        # Creamos un boton para cerrar la ventana
-        boton = tk.Button(ventana, text='Cerrar', command=ventana.destroy)
-        boton.pack(fill=tk.X, padx=5, pady=5)
-
-        ventana.mainloop()
-
-    # Combobox Marca
-    def comboMarca(ventana):
-        # Creamos una lista con 5 marcas de teléfonos celulares
-        lista_marcas = ['Apple', 'Samsung', 'Huawei', 'Xiaomi', 'Motorola']
-
-        # Creamos la función que asignara el valor de la marca al objeto telefono
-        def seleccionMarca(event):
-            telefono.marca = comboMarca.get()
+        if now not in values:
+            self.combo2.set("")
+            self.combo2.config(values=values)
             
-        # combobox marca
-        comboMarca = ttk.Combobox(ventana, values=lista_marcas, state='readonly')
-        comboMarca.pack(fill=tk.X, padx=5, pady=5)
-        comboMarca.bind('<<ComboboxSelected>>', seleccionMarca, telefono)
-
-    def comboModelo(ventana):
-        # Creamos una lista con 5 modelos de teléfonos celulares segun la marca seleccionada
-        if telefono.marca == 'Apple':
-            lista_modelos = ['iPhone 12', 'iPhone 11', 'iPhone X', 'iPhone 8', 'iPhone 7']
-
-        # Creamos la función que asignara el valor del modelo al objeto telefono
-        def seleccionModelo(event):
-            telefono.modelo = comboModelo.get()
-            
-        # combobox modelo
-        comboModelo = ttk.Combobox(ventana, values=lista_modelos, state='readonly')
-        comboModelo.pack(fill=tk.X, padx=5, pady=5)
-        comboModelo.bind('<<ComboboxSelected>>', seleccionModelo, telefono)
-    
-    
-
-    # Ejecutamos la función ventana
-    ventana()
-
 
 # Método principal del programa
 def main():
+    root = tk.Tk()
+    root.geometry("400x400")
+    root.resizable(False, False)
+    #Creamos un titulo con formato
+    root.title("Tienda de celulares")
+    
+    
+    App(root).pack(expand=True, fill=tk.BOTH)
+    root.mainloop()
+
     # Creamos un objeto de la clase TelefonoCelular
     telefono = TelefonoCelular()
 
-    Interfaz1(telefono)
+    #Interfaz1(telefono)
 
     print("marca", telefono.marca)
     print("modelo", telefono.modelo)
